dee74475c6f2860eaa781535bfbf6b67f78e5cf9
Philipp Renoth <renoth@wildoak.de>
2018-05-26 14:53:51 +0200

    http2: reuse `http` tests for `http2` compat
    Simple try refactoring `test/parallel/test-http-request-end.js`
    to run with `http` and `http2`.
    
    Refs: https://github.com/nodejs/node/issues/20868

--- test/parallel/test-http-request-end.js
+++ test/parallel/test-http-request-end.js
@@ -23,40 +23,68 @@
 require('../common');
 const assert = require('assert');
 const http = require('http');
+const http2 = require('http2');
 
-const expected = 'Post Body For Test';
-
-const server = http.Server(function(req, res) {
-  let result = '';
-
-  req.setEncoding('utf8');
-  req.on('data', function(chunk) {
-    result += chunk;
+const http2RequestCompat = (options, cb) => {
+  const sess = http2.connect(`http://localhost:${options.port}`);
+  const stream = sess.request({
+    ':path': options.path,
+    ':method': options.method
+  });
+  
+  stream.on('response', headers => {
+    cb({ // res
+      statusCode: headers[':status'],
+      resume: () => stream.resume()
+    });
   });
 
-  req.on('end', function() {
-    assert.strictEqual(expected, result);
-    server.close();
-    res.writeHead(200);
-    res.end('hello world\n');
+  stream.on('close', () => {
+    sess.close();
   });
 
-});
+  return stream;
+};
 
-server.listen(0, function() {
-  const req = http.request({
-    port: this.address().port,
-    path: '/',
-    method: 'POST'
-  }, function(res) {
-    console.log(res.statusCode);
-    res.resume();
-  }).on('error', function(e) {
-    console.log(e.message);
-    process.exit(1);
+for (const [createServer, request] of [
+  [http.createServer, http.request],
+  [http2.createServer, http2RequestCompat]
+]) {
+
+  const expected = 'Post Body For Test';
+  
+  const server = createServer(function(req, res) {
+    let result = '';
+
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+      result += chunk;
+    });
+
+    req.on('end', function() {
+      assert.strictEqual(expected, result);
+      server.close();
+      res.writeHead(200);
+      res.end('hello world\n');
+    });
+
   });
 
-  const result = req.end(expected);
+  server.listen(0, function() {
+    const req = request({
+      port: this.address().port,
+      path: '/',
+      method: 'POST'
+    }, function(res) {
+      console.log(res.statusCode);
+      res.resume();
+    }).on('error', function(e) {
+      console.log(e.message);
+      process.exit(1);
+    });
 
-  assert.strictEqual(req, result);
-});
+    const result = req.end(expected);
+
+    assert.strictEqual(req, result);
+  });
+}
